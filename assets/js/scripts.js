008f688a526c23908dd64402db4e79b5d853d7f0
max <gleibsonluiz@gmail.com>
2023-11-19 22:46:27 -0300

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+
+const menu = document.querySelector("#btn-menu");
+
+/*console.dir(menu);*/
+
+menu.addEventListener('click', () => {
+    console.log('clicou')
+});
+
+
+
+const sort = document.querySelector('#btn-sort');
+
+sort.addEventListener('click', () => {
+    window.location.href = 'http://class.hcode.com.br';
+})
+
+
+
